10ea7073d67a28b6a72a67c26826c82b53525f05
mikhail.kusachev <mikhail.kusachev@gmail.com>
2025-11-22 09:18:43 +0300

    add new problem and solution: 70 Climbing Stairs

---
+++ 70_ClimbingStairs/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.climbStairs(1));
+        System.out.println(s.climbStairs(2));
+        System.out.println(s.climbStairs(3));
+        System.out.println(s.climbStairs(4));
+        System.out.println(s.climbStairs(5));
+        System.out.println(s.climbStairs(45));
+    }
+}
---
+++ 70_ClimbingStairs/src/main/java/org/example/Solution.java
@@ -0,0 +1,31 @@
+package org.example;
+
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2)
+            return n;
+
+        int oneStepBack = 2;
+        int twoStepBack = 1;
+        int temp;
+
+        for (int i = 3; i <= n; i++) {
+            temp = oneStepBack + twoStepBack;
+            twoStepBack = oneStepBack;
+            oneStepBack = temp;
+        }
+
+        return oneStepBack;
+    }
+}
+
+//if (n == 1)
+//    return 1;
+//
+//int[] dp = new int[n+1];
+//dp[1] = 1;
+//dp[2] = 2;
+//
+//for (int i = 3; i <= n; i++)
+//    dp[i] = dp[i-1] + dp[i-2];
+//return dp[n];
