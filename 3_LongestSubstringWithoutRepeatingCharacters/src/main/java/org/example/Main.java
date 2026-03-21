93dc92e8364f064c6efd498a29393b6987840554
mikhail.kusachev <mikhail.kusachev@gmail.com>
2026-03-21 18:45:53 +0300

    minor changes in previous solution: 3 Longest Substring Without Repeating Characters. Todo: skip tail removing using Last Seen map

--- 3_LongestSubstringWithoutRepeatingCharacters/src/main/java/org/example/Main.java
+++ 3_LongestSubstringWithoutRepeatingCharacters/src/main/java/org/example/Main.java
@@ -2,19 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println(new Solution().lengthOfLongestSubstring("abcabcbb"));
-//        System.out.println(new Solution().lengthOfLongestSubstring("bbbbb"));
-//        System.out.println(new Solution().lengthOfLongestSubstring("pwwkew"));
-//        System.out.println(new Solution().lengthOfLongestSubstring("abcdefghijklmnopqrstuvwxyz"));
-//        System.out.println(new Solution().lengthOfLongestSubstring("abcabcabcabcabcabcabc"));
-//        System.out.println(new Solution().lengthOfLongestSubstring(""));
-//        System.out.println(new Solution().lengthOfLongestSubstring("a"));
-//        System.out.println(new Solution().lengthOfLongestSubstring("ab"));
-//        System.out.println(new Solution().lengthOfLongestSubstring("aa"));
-//        System.out.println(new Solution().lengthOfLongestSubstring("aab"));
-//        System.out.println(new Solution().lengthOfLongestSubstring("cdd"));
-//        System.out.println(new Solution().lengthOfLongestSubstring("abac"));
-//        System.out.println(new Solution().lengthOfLongestSubstring("abcbda"));
-//        System.out.println(new Solution().lengthOfLongestSubstring("abcdeafbdgcbb"));
+        System.out.println("Input: \"\" → Expected: 0 → Output: " + new Solution().lengthOfLongestSubstring(""));
+        System.out.println("Input: \" \" → Expected: 1 → Output: " + new Solution().lengthOfLongestSubstring(" "));
+        System.out.println("Input: \"au\" → Expected: 2 → Output: " + new Solution().lengthOfLongestSubstring("au"));
+        System.out.println("Input: \"dvdf\" → Expected: 3 → Output: " + new Solution().lengthOfLongestSubstring("dvdf"));
+        System.out.println("Input: \"anviaj\" → Expected: 5 → Output: " + new Solution().lengthOfLongestSubstring("anviaj"));
+        System.out.println("Input: \"tmmzuxt\" → Expected: 5 → Output: " + new Solution().lengthOfLongestSubstring("tmmzuxt"));
+        System.out.println("Input: \"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\" → Expected: 52 → Output: " + new Solution().lengthOfLongestSubstring("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        System.out.println("Input: \"aab\" → Expected: 2 → Output: " + new Solution().lengthOfLongestSubstring("aab"));
+        System.out.println("Input: \"abba\" → Expected: 2 → Output: " + new Solution().lengthOfLongestSubstring("abba"));
+        System.out.println("Input: \"abcabcbbdefghijklmnopqrstuvwxyz\" → Expected: 24 → Output: " + new Solution().lengthOfLongestSubstring("abcabcbbdefghijklmnopqrstuvwxyz"));
+        System.out.println("Input: \"pwwkew\" → Expected: 3 → Output: " + new Solution().lengthOfLongestSubstring("pwwkew"));
+        System.out.println("Input: \"bbbbb\" → Expected: 1 → Output: " + new Solution().lengthOfLongestSubstring("bbbbb"));
+        System.out.println("Input: \"abcabcbb\" → Expected: 3 → Output: " + new Solution().lengthOfLongestSubstring("abcabcbb"));
     }
 }
--- 3_LongestSubstringWithoutRepeatingCharacters/src/main/java/org/example/Solution.java
+++ 3_LongestSubstringWithoutRepeatingCharacters/src/main/java/org/example/Solution.java
@@ -5,39 +5,33 @@ import java.util.Set;
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 1) { return 1; }
-        if (s.length() == 0) { return 0; }
-
         Set<Character> set = new HashSet<>();
-        int max = 1;
-        int tail = 0;
-        int head = 1;
-        int current = 1;
-        boolean isGrow = true;
-        Character headCh = s.charAt(0);
-        set.add(headCh);
+        var right = 0;
+        var left = 0;
+        var max = 0;
+        var count = 0;
 
-        while (head < s.length()) {
-            if (isGrow) {
-                Character ch = s.charAt(head++);
-                if (!set.contains(ch)) {
-                    set.add(ch);
-                    current++;
-                } else {
-                    max = Math.max(current, max);
-                    isGrow = false;
-                    headCh = ch;
-                }
+        while (right < s.length()) {
+            if (!set.contains(s.charAt(right))) {
+                count++;
+                set.add(s.charAt(right));
+                right++;
             } else {
-                Character ch = s.charAt(tail++);
-                if (ch != headCh) {
-                    set.remove(ch);
-                    current--;
-                } else
-                    isGrow =  true;
+                if (count > max)
+                    max = count;
+                while (s.charAt(left) != s.charAt(right)) {
+                    set.remove(s.charAt(left));
+                    left++;
+                    count--;
+                }
+                left++;
+                right++;
             }
         }
 
-        return Math.max(current, max);
+        if (count > max)
+            max = count;
+
+        return max;
     }
 }
