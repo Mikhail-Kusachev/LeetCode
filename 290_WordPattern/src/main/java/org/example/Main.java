b4650adcc59c094294ea89dcf0b3ced110306d82
mikhail.kusachev <mikhail.kusachev@gmail.com>
2026-03-23 14:43:35 +0300

    add different solution with manual implementation of bimap: 290 Word Pattern

--- 290_WordPattern/src/main/java/org/example/Main.java
+++ 290_WordPattern/src/main/java/org/example/Main.java
@@ -1,10 +1,7 @@
 package org.example;
 
-
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,21 +11,19 @@ public class Main {
 
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        var words = s.split(" ");
-        if (words.length != pattern.length()) return false;
-        Map<Character, String> map = new HashMap<>();
-        Set<String> set = new HashSet<>();
-        for (int i = 0; i < words.length; i++) {
-            var key = pattern.charAt(i);
-            var value = words[i];
-            if (map.containsKey(key)) {
-                if (!map.get(key).equals(value)) return false;
-            } else {
-                if (set.contains(value)) return false;
-                map.put(key, value);
-                set.add(value);
-            }
+        var arr1 = pattern.toCharArray();
+        var arr2 = s.split(" ");
+        if (arr1.length != arr2.length) return false;
+        Map<Character, String> map1 = new HashMap<>();
+        Map<String, Character> map2 = new HashMap<>();
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (map1.containsKey(arr1[i]) && !map1.get(arr1[i]).equals(arr2[i])) return false;
+            if (map2.containsKey(arr2[i]) && !map2.get(arr2[i]).equals(arr1[i])) return false;
+            map1.putIfAbsent(arr1[i], arr2[i]);
+            map2.putIfAbsent(arr2[i], arr1[i]);
         }
+
         return true;
     }
 }
