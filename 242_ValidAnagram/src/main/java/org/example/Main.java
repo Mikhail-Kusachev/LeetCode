81b73e8303f8211c0a090c7b9fbc919a99e8315b
mikhail.kusachev <mikhail.kusachev@gmail.com>
2026-03-23 13:24:05 +0300

    add new solution with normal algorithm not a library sort: 242 Valid Anagram

--- 242_ValidAnagram/src/main/java/org/example/Main.java
+++ 242_ValidAnagram/src/main/java/org/example/Main.java
@@ -1,20 +1,25 @@
 package org.example;
 
-import static java.util.Arrays.stream;
-import static java.util.stream.Collectors.joining;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "\u0048\u0065\u006C\u006C\u006F";
-        String t = "\u006C\u0065\u0048\u006F\u006C";
-        //String t = "\u0057\u006F\u0072\u006C\u0064";
+        String s = "hella";
+        String t = "lleoh";
         System.out.println(new Solution().isAnagram(s, t));
     }
 }
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-        return stream(s.split("")).sorted().collect(joining())
-                .equals(stream(t.split("")).sorted().collect(joining()));
+        if (s.length() != t.length())
+            return false;
+        var dict = new int[26];
+        for (var i = 0; i < s.length(); i++) {
+            dict[s.charAt(i) - 'a']++;
+            dict[t.charAt(i) - 'a']--;
+        }
+
+        return Arrays.equals(dict, new int[26]);
     }
 }
