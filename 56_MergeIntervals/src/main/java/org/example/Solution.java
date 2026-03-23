790243475183b9ee6c65598de6da1fefbffdd2ad
mikhail.kusachev <mikhail.kusachev@gmail.com>
2026-03-23 18:39:35 +0300

    add new solution with sort, two pointers, in-place writing: 56 Merge Intervals

--- 56_MergeIntervals/src/main/java/org/example/Solution.java
+++ 56_MergeIntervals/src/main/java/org/example/Solution.java
@@ -1,36 +1,22 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 class Solution {
     public int[][] merge(int[][] intervals) {
-        int len = intervals.length;
+        Arrays.sort(intervals, Comparator.comparingInt(e -> e[0]));
+        int write = 0;
 
-        boolean isChanges;
-        do {
-            isChanges = false;
-            for (int i = 0; i < intervals.length - 1; i++) {
-                if (intervals[i][0] == -1) continue;
-                for (int j = i + 1; j < intervals.length; j++) {
-                    if (intervals[j][0] == -1) continue;
-                    if (intervals[i][0] <= intervals[j][1] && intervals[j][0] <= intervals[i][1]) {
-                        intervals[i][0] = Math.min(intervals[i][0], intervals[j][0]);
-                        intervals[i][1] = Math.max(intervals[i][1], intervals[j][1]);
-                        intervals[j][0] = -1;
-                        len--;
-                        isChanges = true;
-                    }
-                }
-            }
-        } while (isChanges);
-
-        int[][] result = new int[len][2];
-        int index = 0;
-        for (int i = 0; i < intervals.length; i++){
-            if (intervals[i][0] != -1) {
-                result[index][0] = intervals[i][0];
-                result[index++][1] = intervals[i][1];
+        for (int read = 1; read < intervals.length; read++) {
+            if (intervals[write][1] >= intervals[read][0]) {
+                intervals[write][1] = Math.max(intervals[write][1], intervals[read][1]);
+            } else {
+                write++;
+                intervals[write] = intervals[read];
             }
         }
 
-        return result;
+        return Arrays.copyOfRange(intervals, 0, write + 1);
     }
 }
