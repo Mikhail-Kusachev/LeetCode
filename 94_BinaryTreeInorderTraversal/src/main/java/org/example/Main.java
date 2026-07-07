55dea85549e748c082a643da0b444bce153662cc
mikhail.kusachev <mikhail.kusachev@gmail.com>
2026-07-07 13:51:57 +0300

    add new problem and recursive solution: 94 Binary Tree Inorder Traversal. TODO: iterative solution

---
+++ 94_BinaryTreeInorderTraversal/src/main/java/org/example/Main.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        /*TreeNode node3 = new TreeNode(3, null, null);
+        TreeNode node2 = new TreeNode(2, node3, null);
+        TreeNode node1 = new TreeNode(1, null, node2);*/
+
+        TreeNode node9 = new TreeNode(9, null, null);
+        TreeNode node8 = new TreeNode(8, node9, null);
+        TreeNode node7 = new TreeNode(7, null, null);
+        TreeNode node6 = new TreeNode(6, null, null);
+        TreeNode node5 = new TreeNode(5, node6, node7);
+        TreeNode node4 = new TreeNode(4, null, null);
+        TreeNode node3 = new TreeNode(3, null, node8);
+        TreeNode node2 = new TreeNode(2, node4, node5);
+        TreeNode node1 = new TreeNode(1, node2, node3);
+
+        System.out.println(s.inorderTraversal(node1));
+    }
+}
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(result, root);
+        return  result;
+    }
+
+    private void inorder(List<Integer> list, TreeNode root) {
+        if (root == null) return;
+        inorder(list, root.left);
+        list.add(root.val);
+        inorder(list, root.right);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+ }
