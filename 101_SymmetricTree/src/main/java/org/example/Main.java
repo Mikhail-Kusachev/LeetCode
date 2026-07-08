cbbe4c17299aba5ff1eaa6d5fd546e7b9ff1acfc
mikhail.kusachev <mikhail.kusachev@gmail.com>
2026-07-08 18:32:51 +0300

    add new problem and iterative solution: 101 Symmetric Tree

--- 101_SymmetricTree/src/main/java/org/example/Main.java
+++ 101_SymmetricTree/src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 class TreeNode {
     int val;
     TreeNode left;
@@ -25,11 +29,11 @@ public class Main {
         TreeNode node11 = new TreeNode(2, node21, node22);
         TreeNode node1 = new TreeNode(1, node11, node12);
 
-//        TreeNode node24 = new TreeNode(3);
-//        TreeNode node22 = new TreeNode(3);
-//        TreeNode node12 = new TreeNode(2, null, node24);
-//        TreeNode node11 = new TreeNode(2, null, node22);
-//        TreeNode node1 = new TreeNode(1, node11, node12);
+        /*TreeNode node24 = new TreeNode(3);
+        TreeNode node22 = new TreeNode(3);
+        TreeNode node12 = new TreeNode(2, null, node24);
+        TreeNode node11 = new TreeNode(2, null, node22);
+        TreeNode node1 = new TreeNode(1, node11, node12);*/
 
         System.out.println(s.isSymmetric(node1));
     }
@@ -37,16 +41,24 @@ public class Main {
 
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null)return true;
+        if (root == null) return true;
+        Deque<TreeNode> stack = new LinkedList<>();
 
-        return isMirror(root.left, root.right);
-    }
+        stack.push(root.left);
+        stack.push(root.right);
+        while (!stack.isEmpty()) {
+            TreeNode left = stack.pop();
+            TreeNode right = stack.pop();
 
-    private boolean isMirror(TreeNode left, TreeNode right) {
-        if (left == null && right == null) return true;
-        if (left == null || right == null) return false;
-        if (left.val != right.val) return false;
+            if (left == null && right == null) continue;
+            if (left == null || right == null || left.val != right.val) return false;
 
-        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+            stack.push(left.left);
+            stack.push(right.right);
+            stack.push(left.right);
+            stack.push(right.left);
+        }
+
+        return true;
     }
 }
